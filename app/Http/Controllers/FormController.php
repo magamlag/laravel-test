f3bb5919359ac7ab7e0b8b0f2dfa3781b9b4ac65
magamlag <lagovskiy@gmail.com>
2016-10-28 16:47:24 -0400

    Handle exceptions. Check if file exist before write something inside.

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use Illuminate\Support\Facades\File;
+use Illuminate\Filesystem\Filesystem;
+
 class FormController extends Controller
 {
     /**
@@ -24,15 +28,20 @@ class FormController extends Controller
      *
      * @return mixed
      */
-    public function saveForm(Request $request){
+    public function saveForm( Request $request ) {
+        try {
+            $path = public_path( 'product.json' );
+            if ( !File::exists( $path ) ) {
+                return response()->json( [ 'error' => 'Fail to wrote data. File doesn\'t exist' ] );
+            }
+            $data_array = array_merge( $request->all(), [ 'date_time' => Carbon::now()->toDateTimeString() ] );
+            $data_json  = json_encode( $data_array );
 
-        if(!Storage::exists('product_json')){
-            return response()->json(['error'=> 'Fail to wrote data. File doesn\'t exist']);
+            Storage::append( 'product.json', $data_json );
+
+            return response()->json( $data_json );
+        } catch ( Exception $e ) {
+            return $e->getMessage();
         }
-
-        $data = $request->all();
-        Storage::disk('local')->put('product_json.txt', $data);
-
-        return response()->json_encode($data);
     }
 }
